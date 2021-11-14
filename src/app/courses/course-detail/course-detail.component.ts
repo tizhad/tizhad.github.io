4feff09da8c0d333f345cda705c3f600ff962e7c
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2021-11-14 23:23:26 +0100

    Add course detail to show the course that user select.

--- src/app/courses/course-detail/course-detail.component.ts
+++ src/app/courses/course-detail/course-detail.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-course-detail',
-  templateUrl: './course-detail.component.html',
-  styleUrls: ['./course-detail.component.scss']
+    selector: 'app-course-detail',
+    templateUrl: './course-detail.component.html',
+    styleUrls: ['./course-detail.component.scss']
 })
 export class CourseDetailComponent implements OnInit {
+    course!: { id: number, title: string, image: string, description: string }
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) {
+        console.log(this.course)
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.course = {
+            id: this.route.snapshot.params['id'],
+            title: this.route.snapshot.params['title'],
+            image: this.route.snapshot.params['image'],
+            description: this.route.snapshot.params['description'],
+        }
+    }
 
 }
