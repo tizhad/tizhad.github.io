4cb666a32c73af455f6413462460a99cf335024e
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2021-11-14 23:22:45 +0100

    Add router outlet to the app component to mange routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CourseDetailComponent} from "./courses/course-detail/course-detail.component";
+import {CoursesComponent} from "./courses/courses.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+    {path: '', component: CoursesComponent},
+    {path: 'detail/:id/:title', component: CourseDetailComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
