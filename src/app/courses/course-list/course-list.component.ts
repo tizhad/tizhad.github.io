75cf69817d0f025ade95497d78f7cfaf4242ee47
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-01-18 00:42:58 +0100

    Add dummy data to the course list

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-
+  courseList = [
+    {id: 1, name: 'The Web Developer Bootcamp 2022', description: 'Good', rate: 4},
+    {id: 2, name: 'HTML & CSS', description: 'Awesome', rate: 4.2},
+    {id: 3, name: 'Javascript 2021', description: 'Awesome', rate: 4}
+  ];
   constructor() { }
+  onCourseAdded(newCourseData: {name: string, description: string, rate: number}){
+    this.courseList.push({
+      id: 6,
+      name: newCourseData.name,
+      description: newCourseData.description,
+      rate: newCourseData.rate
+    })
+  }
+
 
   ngOnInit(): void {
   }
