45f90eb75b6ad0240f5698c9adb308e95ee2b1da
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-01-18 00:41:14 +0100

    Edit courses as a card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {CourseDetailComponent} from "./courses/course-detail/course-detail.component";
+import {CourseListComponent} from "./courses/course-list/course-list.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
+import {NewCourseComponent} from "./courses/new-course/new-course.component";
 
 const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'course-detail/:id', component: CourseDetailComponent},
+    {path: 'courses', component: CourseListComponent},
+    {path: 'new-course', component: NewCourseComponent},
+    {path: 'join-us', component: NewCourseComponent},
+
 ];
 
 @NgModule({
