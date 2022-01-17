be947be8ea3512ff55d648081386fcb2990f4552
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-01-18 00:40:31 +0100

    Remove dummy data from app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "tizhad";
-  courseList = [
-      {name: 'The Web Developer Bootcamp 2022', description: 'Good', rate: 4},
-    {name: 'HTML & CSS', description: 'Awesome', rate: 4.2},
-    {name: 'Javascript 2021', description: 'Awesome', rate: 4}
-  ];
 
-  onCourseAdded(newCourseData: {name: string, description: string, rate: number}){
-    this.courseList.push({
-      name: newCourseData.name,
-      description: newCourseData.description,
-      rate: newCourseData.rate
-    })
-  }
+  title = "Tizhad";
 }
