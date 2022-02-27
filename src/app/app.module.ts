1a9a2ddbd2ec6b21729065413528f4d09e747f85
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-02-27 18:34:49 +0100

    A bunch of changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { CourseComponent } from './courses/course/course.component';
-import { CourseListComponent } from './courses/course-list/course-list.component';
-import { NewCourseComponent } from './courses/new-course/new-course.component';
-import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        ShoppingListComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        ShoppingEditComponent,
-        CourseComponent,
-        CourseListComponent,
-        NewCourseComponent,
-        CourseDetailComponent,
         HomeComponent,
     ],
     imports: [
